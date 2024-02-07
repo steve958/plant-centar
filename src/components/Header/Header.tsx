54b85c21550e296c78eb2977e7c4286def1ce574
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2024-02-07 17:37:05 +0100

    map update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,11 @@
-import './Header.css'
-import FmdGoodIcon from '@mui/icons-material/FmdGood';
-import SearchIcon from '@mui/icons-material/Search';
-import { useEffect, useState } from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import "./Header.css";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+// import SearchIcon from '@mui/icons-material/Search';
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Header() {
-
-    const [showMap, setShowMap] = useState<boolean>(false)
+    const [showMap, setShowMap] = useState<boolean>(false);
 
     useEffect(() => {
         if (showMap) {
@@ -17,35 +16,42 @@ export default function Header() {
         }
     }, [showMap]);
 
-    function toggleShowMap() {
-        const old = showMap
-        setShowMap(!old)
-    }
 
     return (
         <div className="header-container">
             <div className="header-wrapper">
-                <p className='moto'><b>Posvećenost uspehu</b></p>
-                <span className='address-wrapper'>
-                    <FmdGoodIcon color='secondary' className='icon' onClick={() => toggleShowMap()}></FmdGoodIcon>
+                <p className="moto">
+                    <b>Posvećenost uspehu</b>
+                </p>
+                <span className="address-wrapper">
+                    <FmdGoodIcon
+                        color="secondary"
+                        className="icon"
+                        onClick={() => setShowMap(true)}
+                    ></FmdGoodIcon>
                     <p>Vojvode Janka Stojićevića 22, Šabac</p>
                 </span>
-                <div className="search-wrapper">
+                {/* <div className="search-wrapper">
                     <SearchIcon color='primary' className='icon'></SearchIcon>
-                </div>
+                </div> */}
             </div>
-            {showMap && <div className='map-container'>
-                <MapContainer center={[44.746476, 19.7033064]} zoom={13} scrollWheelZoom={false}>
-                    <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[44.746476, 19.7033064]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
-                </MapContainer>
-            </div>}
+            {showMap && (
+                <>
+                    <div className="map-container">
+                        <MapContainer
+                            center={[44.746476, 19.7033064]}
+                            zoom={14}
+                            scrollWheelZoom={false}
+                        >
+                            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                            <Marker position={[44.746476, 19.7033064]}>
+                                <Popup>Plant-centar</Popup>
+                            </Marker>
+                        </MapContainer>
+                        <p className="close-map" onClick={() => setShowMap(false)}>zatvori mapu</p>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
