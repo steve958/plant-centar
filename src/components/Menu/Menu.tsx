0b675be233db6d701871e6083bfa0dccc61a9402
steve958 <stevenex958@gmail.com>
2024-02-16 18:14:01 +0100

    categories update

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,43 +1,83 @@
-import { useState } from "react";
-import "./Menu.css";
-import MenuIcon from "@mui/icons-material/Menu";
-import logoMain from "../../assets/PlantCLogo.jpg";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useState } from 'react';
+import './Menu.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import logoMain from '../../assets/PlantCLogo.jpg';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export default function Menu() {
-    const [menuClicked, setMenuClicked] = useState<boolean>(false);
+  const [menuClicked, setMenuClicked] = useState<boolean>(false);
+  const [categoriesHover, setCategoriesHover] = useState<boolean>(false);
 
-    function toggleMenu() {
-        const oldState = menuClicked;
-        setMenuClicked(!oldState);
-    }
+  function toggleMenu() {
+    const oldState = menuClicked;
+    setMenuClicked(!oldState);
+  }
 
-    return (
-        <div className="menu-container">
-            <div className="menu-wrapper">
-                <div className="menu-item">Početna</div>
-                <div className="menu-item">O nama</div>
-                <div className="menu-item">Naš asortiman</div>
-                <div className="menu-item">Galerija</div>
-                <div className="menu-item">Kontakt</div>
+  function hoverIn() {
+    setCategoriesHover(true);
+  }
+
+  function hoverOut() {
+    setCategoriesHover(false);
+  }
+
+  return (
+    <div className="menu-container">
+      <div className="menu-wrapper">
+        <div className="menu-item">Početna</div>
+        <div className="menu-item">O nama</div>
+        <div
+          className="menu-item"
+          onMouseEnter={hoverIn}
+          onMouseLeave={hoverOut}
+        >
+          Naš asortiman
+          <KeyboardArrowDownIcon />
+          {categoriesHover && (
+            <div className="categories-container">
+              <div className="category">bla</div>
+              <div className="category">bla</div>
+              <div className="category">bla</div>
+              <div className="category">bla</div>
             </div>
-            <span onClick={toggleMenu} className="menu-icon">
-                {!menuClicked ? (
-                    <MenuIcon />
-                ) : (
-                    <ArrowBackIcon id={menuClicked ? "active" : "inactive"} />
-                )}
-                <img src={logoMain} alt="" />
-            </span>
-            {menuClicked && (
-                <div className="small-screen-wrapper">
-                    <div className="small-menu-item">Početna</div>
-                    <div className="small-menu-item">O nama</div>
-                    <div className="small-menu-item">Naš asortiman</div>
-                    <div className="small-menu-item">Galerija</div>
-                    <div className="small-menu-item">Kontakt</div>
-                </div>
-            )}
+          )}
         </div>
-    );
+        <div className="menu-item">Galerija</div>
+        <div className="menu-item">Kontakt</div>
+      </div>
+      <span onClick={toggleMenu} className="menu-icon">
+        {!menuClicked ? (
+          <MenuIcon />
+        ) : (
+          <ArrowBackIcon id={menuClicked ? 'active' : 'inactive'} />
+        )}
+        <img src={logoMain} alt="" />
+      </span>
+      {menuClicked && (
+        <div className="small-screen-wrapper">
+          <div className="small-menu-item">Početna</div>
+          <div className="small-menu-item">O nama</div>
+          <div
+            className="small-menu-item"
+            onMouseEnter={hoverIn}
+            onMouseLeave={hoverOut}
+          >
+            Naš asortiman
+            <KeyboardArrowDownIcon />
+            {categoriesHover && (
+              <div className="small-categories-container">
+                <div className="category">bla</div>
+                <div className="category">bla</div>
+                <div className="category">bla</div>
+                <div className="category">bla</div>
+              </div>
+            )}
+          </div>
+          <div className="small-menu-item">Galerija</div>
+          <div className="small-menu-item">Kontakt</div>
+        </div>
+      )}
+    </div>
+  );
 }
