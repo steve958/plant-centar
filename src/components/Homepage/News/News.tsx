c90231a035bdc416ab6856cbff54626b73eee11e
Stefan Miljevic <77570817+steve958@users.noreply.github.com>
2025-02-08 11:46:20 +0100

    build fail fix

--- src/components/Homepage/News/News.tsx
+++ src/components/Homepage/News/News.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./News.css";
 import { newsData } from "./newsData"; // Import your mock data
 import NewsCard from "./NewsCard";
